2263ce74011e5509f6b846bc6d4b7fdc7b165667
Kéwan Bernier <mail@kewan.fr>
2023-02-12 20:37:38 +0100

    fix(dbname conf change)

--- core/Model.php
+++ core/Model.php
@@ -3,7 +3,7 @@ class Model{
 
     static $connections = array();
 
-    public $conf = Conf::$dbName;
+    public $conf = 'default';
     public $table = false;
     public $db;
     public $primaryKey = 'id';
