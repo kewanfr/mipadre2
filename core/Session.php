44712603ea595ae15e04352f1074d8ebbe47d109
Kéwan Bernier <mail@kewan.fr>
2023-02-12 17:49:19 +0100

    Session retain flash msg

--- core/Session.php
+++ core/Session.php
@@ -6,35 +6,46 @@ class Session{
     }  
   }
 
-  public function setFlash($message, $type = 'success'){
-    if(!isset($_SESSION['flash'])){
-      $_SESSION['flash'] = array();
-    }
-    $_SESSION['flash'][0] = array(
-      'message' => $message,
-      'type' => $type
-    );
-  }
-
-  public function addFlashMessage($message, $type = 'success'){
+  public function setFlash($message, $type = 'success', $retain = null){
     if(!isset($_SESSION['flash'])){
       $_SESSION['flash'] = array();
     }
     $_SESSION['flash'][] = array(
       'message' => $message,
-      'type' => $type
+      'type' => $type,
+      'retain' => $retain
+    );
+  }
+
+  public function addFlashMessage($message, $type = 'success', $retain = null){
+    if(!isset($_SESSION['flash'])){
+      $_SESSION['flash'] = array();
+    }
+    $_SESSION['flash'][] = array(
+      'message' => $message,
+      'type' => $type,
+      'retain' => $retain
     );
   }
 
   public function flash(){
     if(isset($_SESSION['flash']) and is_array($_SESSION['flash']) and !empty($_SESSION['flash']) ){
       $html = '';
-      foreach($_SESSION['flash'] as $flash){
+      foreach($_SESSION['flash'] as $key => $flash){
         $html .= '<div class="alert alert-'.$flash['type'].'">';
         $html .= $flash['message'].'<br>';
         $html .= '</div>';
+        if(!$flash['retain']){
+          unset($_SESSION['flash'][$key]);
+        }else {
+          if($flash['retain'] == 'once'){
+            unset($_SESSION['flash'][$key]['retain']);
+          }else {
+            $_SESSION['flash'][$key]['retain'] = $flash['retain'] - 1;
+          }
+        }
       }
-      $_SESSION['flash'] = array();
+      // $_SESSION['flash'] = array();
       return $html;
     }
   }
@@ -57,7 +68,25 @@ class Session{
   }
 
   public function isLogged(){
-    return isset($_SESSION['User']->ID);
+    return isset($_SESSION['User']->id);
+  }
+
+  public function isLoggedAs($type = "user"){
+    debug($_SESSION);
+    switch ($type) {
+      case 'admin':
+        return isset($_SESSION['User']->id) && $_SESSION['User']->role == 'admin';
+        break;
+      case 'user':
+        return isset($_SESSION['User']->id);
+        break;
+      case 'guest':
+        return isset($_SESSION['Guest']->id);
+        break;
+      default:
+        return false;
+        break;
+    }
   }
 
   public function user($key){
