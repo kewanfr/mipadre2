579a7874a822d6ac30f5d902b313eb1db5d767e2
Kéwan Bernier <mail@kewan.fr>
2023-02-12 15:26:39 +0100

    error pages

---
+++ view/errors/401.php
@@ -0,0 +1,8 @@
+<br>
+<?php $title_for_layout = 'Erreur 401 - Non Autorisé'; ?>
+<div class="hero-unit-danger alert alert-danger">
+    <div class="">
+        <h1>Non Autorisé</h1>
+        <p><?php echo $message; ?></p>
+    </div>
+</div>
---
+++ view/errors/404.php
@@ -0,0 +1,8 @@
+<br>
+<?php $title_for_layout = 'Erreur 404 - Page Introuvable'; ?>
+<div class="hero-unit-danger alert alert-danger">
+    <div class="">
+        <h1>Page Introuvable</h1>
+        <p><?php echo $message; ?></p>
+    </div>
+</div>
